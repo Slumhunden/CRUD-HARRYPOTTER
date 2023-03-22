8254cf4584e5abf92475bcbee120378a9c141a2d
Slumhunden <kroghbastin@gmail.com>
2023-03-22 13:21:03 +0100

    start css samt objekter

--- script.js
+++ script.js
@@ -10,40 +10,35 @@ function initApp() {
     ability: "Intidmidate",
     image: "https://img.pokemondb.net/artwork/large/gyarados.jpg",
     footprint: null,
-    dexindex: "130",
+    dexindex: 130,
     type: "water",
     subtype: "flying",
     weaknesses: "electric, rock moves",
     gender: "male eller female",
-    weight: "235 kg",
-    height:"650 cm",
-    generation:"1",
-    spilversion:"",
-    canEvolve: 
+    weight: 235000,
+    height:650,
+    generation: 1,
+    gameversion:1,
+    canEvolve: true,
+    statsHP: 95,
+    statsAttack:125,
+    statsDefence: 79,
+    statsSpecialAttack: 60,
+    statsSpecialDefence:100,
+    statsSpeed: 81,
   };
   function fetchJSON(){}
   
-  function showPokemon(){}
-  showPokemons(
-    potterImage,
-    potterName,
-    potterGender,
-    potterHouse,
-    potterDateOfBirth,
-    potterAncestry,
-    potterEyeColour,
-    potterHairColour,
-    potterActor
-  );
+  function showPokemon(pokemon){}
+  showPokemons(gyarados)
 }
-function showPokemons(image, name, house, dateOfBirth, actor) {
-  console.log(image, name, house, dateOfBirth, actor);
+function showPokemons(image, name,dexindex,type) {
   const myHTML = /*HTML*/ `
-<article><img src=${image}>
-                <h2>${name}</h2>
-                <p>${house}</p>
-                <p>${dateOfBirth}</p>
-                <p>${actor}</p>
+<article><img src=${pokemon.image}>
+                <h2>${pokemon.name}</h2>
+                <p>${pokemon.house}</p>
+                <p>${pokemon.dexindex}</p>
+                <p>${pokemon.type}</p>
             </article>
             `;
   document.querySelector("#characters").insertAdjacentHTML("beforeend", myHTML);
