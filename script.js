abb3337b0e35d8eba71b77c001335ff4f940edd6
Slumhunden <kroghbastin@gmail.com>
2023-03-23 08:44:12 +0100

    liste opdateret og næsten færdig dialog box

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 window.addEventListener("load", initApp);
 
 function initApp() {
@@ -16,23 +16,20 @@ function initApp() {
     weaknesses: "electric, rock moves",
     gender: "male eller female",
     weight: 235000,
-    height:650,
+    height: 650,
     generation: 1,
-    gameversion:1,
+    gameversion: 1,
     canEvolve: true,
     statsHP: 95,
-    statsAttack:125,
+    statsAttack: 125,
     statsDefence: 79,
     statsSpecialAttack: 60,
-    statsSpecialDefence:100,
+    statsSpecialDefence: 100,
     statsSpeed: 81,
   };
-  
-  showPokemon(gyarados)
-  showPokemon(gyarados)
-  showPokemon(gyarados)
-
+  showPokemon(gyarados);
 }
+
 function showPokemon(pokemon) {
   const myHTML = /*HTML*/ `
             <article class="grid-item">
@@ -41,7 +38,36 @@ function showPokemon(pokemon) {
                 <p>${pokemon.type}</p>
                 <p>${pokemon.dexindex}</p>
                 <p>${pokemon.subtype}</p>
-            </article>
-            `;
-  document.querySelector("#pokemon").insertAdjacentHTML("beforeend", myHTML);
+                </article>
+                `;
+                document.querySelector("#pokemon").insertAdjacentHTML("beforeend", myHTML);
+                document
+                  .querySelector("#pokemon article:last-child")
+                  .addEventListener("click", pokemonClicked);
+                  
+    function pokemonClicked(pokemon) {
+      document.querySelector("#dialog-name").textContent = pokemon.name;
+      document.querySelector("#description").textContent = pokemon.description;
+      document.querySelector("#ability").textContent = pokemon.ability;
+      document.querySelector("#footprint").textContent = pokemon.footprint;
+      document.querySelector("#dexindex").textContent = pokemon.dexindex;
+      document.querySelector("#type").textContent = pokemon.type;
+      document.querySelector("#subtype").textContent = pokemon.subtype;
+      document.querySelector("#weaknesses").textContent = pokemon.weaknesses;
+      document.querySelector("#gender").textContent = pokemon.gender;
+      document.querySelector("#weight").textContent = pokemon.weight;
+      document.querySelector("#height").textContent = pokemon.height;
+      document.querySelector("#generation").textContent = pokemon.generation;
+      document.querySelector("#gameversion").textContent = pokemon.gameversion;
+      document.querySelector("#canEvolve").textContent = pokemon.canEvolve;
+      document.querySelector("#statsHP").textContent = pokemon.statsHP;
+      document.querySelector("#statsAttack").textContent = pokemon.statsAttack;
+      document.querySelector("#statsDefence").textContent = pokemon.statsDefence;
+      document.querySelector("#statsSpecialAttack").textContent =
+        pokemon.statsSpecialAttack;
+      document.querySelector("#statsSpecialDefence").textContent =
+        pokemon.statsSpecialDefence;
+      document.querySelector("#statsSpeed").textContent = pokemon.statsSpeed;
+      document.querySelector("#pokemonbox").showModal()
+  }
 }
