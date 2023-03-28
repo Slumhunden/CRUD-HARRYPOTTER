3be7227658714ff2d7dac865c579af197494aa08
Slumhunden <kroghbastin@gmail.com>
2023-03-28 09:36:48 +0200

    ny json fil

--- script.js
+++ script.js
@@ -2,36 +2,15 @@
 window.addEventListener("load", initApp);
 
 async function initApp() {
-  // Pokémon
-  const gyarados = {
-    // name: "Gyarados",
-    // description:
-    //   "Rarely seen in the wild. Huge and vicious, it is capable of destroying entire cities in a rage.",
-    // ability: "Intidmidate",
-    // image: "https://img.pokemondb.net/artwork/large/gyarados.jpg",
-    // footprint: null,
-    // dexindex: 130,
-    // type: "water",
-    // subtype: "flying",
-    // weaknesses: "electric, rock moves",
-    // gender: "male eller female",
-    // weight: 235000,
-    // height: 650,
-    // generation: 1,
-    // gameversion: 1,
-    // canEvolve: true,
-    // statsHP: 95,
-    // statsAttack: 125,
-    // statsDefence: 79,
-    // statsSpecialAttack: 60,
-    // statsSpecialDefence: 100,
-    // statsSpeed: 81,
-  };
-  const pokemon = await getPokemon();
-  showPokemon(pokemon);
+
+  const pokemons = await getPokemon("pokemons.json");
+  for (const pokemon of pokemons){
+    
+    showPokemon(pokemon);
+  }
 }
-async function getPokemon(){
-  const response = await fetch ("gyarados.json")
+async function getPokemon(url){
+  const response = await fetch (url)
   const data = await response.json()
   return data 
 }
@@ -41,7 +20,7 @@ function showPokemon(pokemon) {
                 <img src="${pokemon.image}">
                 <h2>Name: ${pokemon.name}</h2>
                 <p>Type: ${pokemon.type}</p>
-                <p>Dexindex: ${pokemon.dexindex}</p>
+                <p>Dexindex: ${pokemon.dexIndex}</p>
                 <p>Subtype: ${pokemon.subtype}</p>
                 </article>
                 `;
@@ -61,7 +40,7 @@ function showPokeModal(pokemon){
           pokemon.description;
         document.querySelector("#dialog-ability").textContent = "Ability: " + pokemon.ability;
         document.querySelector("#dialog-footprint").textContent = "Footprint: " + pokemon.footprint;
-        document.querySelector("#dialog-dexindex").textContent = "#" + pokemon.dexindex;
+        document.querySelector("#dialog-dexIndex").textContent = "#" + pokemon.dexindex;
         document.querySelector("#dialog-type").textContent = "Type: " + pokemon.type;
         document.querySelector("#dialog-subtype").textContent = "Subtype: " + pokemon.subtype;
         document.querySelector("#dialog-weaknesses").textContent = "Weaknesses: " + pokemon.weaknesses;
