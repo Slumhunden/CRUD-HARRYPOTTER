419d902ed6b89e3801c36efa5f1df07c496ce989
Slumhunden <kroghbastin@gmail.com>
2023-03-30 11:07:29 +0200

    optimering af json

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ window.addEventListener("load", initApp);
 
 async function initApp() {
 
-  const pokemons = await getPokemon("pokemons.json");
+  const pokemons = await getPokemon(
+    "https://cederdorff.github.io/dat-js/05-data/pokemons.json"
+  );
   for (const pokemon of pokemons){
     
     showPokemon(pokemon);
